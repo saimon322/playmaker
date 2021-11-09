b35e493d5c3f6d5545ec8fb4473322537e79f790
Alex <shshchkn@yandex.ru>
2021-11-09 15:43:39 +0200

    Start icons

--- assets/src/assets/js/spec/svg-sprite.js
+++ assets/src/assets/js/spec/svg-sprite.js
@@ -1,1 +1,7 @@
-import "@icons/logo.svg"
+import "@icons/logo.svg";
+import "@icons/dot.svg";
+import "@icons/facebook.svg";
+import "@icons/instagram.svg";
+import "@icons/twitter.svg";
+import "@icons/vk.svg";
+import "@icons/youtube.svg";
