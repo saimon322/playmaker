4b91a2a9a4010bfec3054dce3977eac8e1d78d02
Alex <shshchkn@yandex.ru>
2021-11-16 16:01:18 +0200

    Header account dropdown

--- assets/src/assets/js/routes/common.js
+++ assets/src/assets/js/routes/common.js
@@ -1,8 +1,10 @@
 import headerMenu from '../utils/headerMenu';
+import headerAccount from '../utils/headerAccount';
 
 export default {
     init() {
         headerMenu();
+        headerAccount();
     },
     finalize() {}
 }
---
+++ assets/src/assets/js/utils/headerAccount.js
@@ -0,0 +1,23 @@
+export default function () {
+    const showAccountDropdown = document.querySelector('.js-show-account');
+    const accountDropdown = document.querySelector('.js-dropdown');
+
+    showAccountDropdown && showAccountDropdown.addEventListener('click', function(e) {
+       e.preventDefault();
+       if (window.innerWidth >= 1200) {
+           const w = this.scrollWidth;
+
+           this.classList.toggle('active');
+           accountDropdown.style.width = w + 'px';
+           accountDropdown.classList.toggle('active');
+       }
+       return false;
+    });
+
+    document.addEventListener('click', e => {
+        if (!accountDropdown.contains(e.target) && !showAccountDropdown.contains(e.target)) {
+            showAccountDropdown.classList.remove('active');
+            accountDropdown.classList.remove('active');
+        }
+    });
+}
