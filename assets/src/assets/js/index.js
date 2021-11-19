03184f28b9d862fbd86f2d02b802b586947c5a84
Alex <shshchkn@yandex.ru>
2021-11-19 16:26:40 +0200

    Scripts update

--- assets/src/assets/js/index.js
+++ assets/src/assets/js/index.js
@@ -5,12 +5,14 @@ import Router from './utils/Router';
 import common from './routes/common';
 import home from './routes/home';
 import account from './routes/account';
+import forecasts from "./routes/forecasts";
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
     common,
     home,
-    account
+    account,
+    forecasts
 })
 
 window.addEventListener("DOMContentLoaded", () => {
