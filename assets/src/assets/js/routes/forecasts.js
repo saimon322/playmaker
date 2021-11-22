4d1176fd504c47357ba4b5b20bd3904cba14e894
Alex <shshchkn@yandex.ru>
2021-11-22 12:47:39 +0200

    Forecasts page - winners

--- assets/src/assets/js/routes/forecasts.js
+++ assets/src/assets/js/routes/forecasts.js
@@ -1,8 +1,10 @@
 import select from '../utils/select';
+import accordion from '../utils/accordion';
 
 export default {
     init() {
         select();
+        accordion();
     },
     finalize() {}
 }
---
+++ assets/src/assets/js/utils/accordion.js
@@ -0,0 +1,30 @@
+export default function () {
+    const accordionBtns = document.getElementsByClassName('js-accordion-btn');
+    const accordionPanels = document.getElementsByClassName('js-accordion-panel');
+
+    accordionBtns && [...accordionBtns].forEach(acc => {
+        acc.addEventListener('click', e => {
+            e.preventDefault();
+
+            e.target.classList.toggle("active");
+            const panel = e.target.closest('.js-accordion').querySelector('.js-accordion-panel');
+
+            console.log(panel);
+
+            for (let i = 0; i < accordionBtns.length; i++) {
+                !accordionBtns[i].contains(e.target) && accordionBtns[i].classList.remove('active');
+            }
+
+            for (let i = 0; i < accordionPanels.length; i++) {
+                accordionPanels[i].style.maxHeight = !accordionPanels[i].contains(panel) && null;
+            }
+
+
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    });
+}
